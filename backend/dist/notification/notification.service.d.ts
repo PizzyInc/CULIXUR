a7e16ec7616cfa2af38851c62d6014246fbeef05
PIZZY INCORPORATIONS <149484882+PizzyInc@users.noreply.github.com>
2026-01-06 13:04:44 +0100

    Delete backend directory

--- backend/dist/notification/notification.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaService } from '../prisma/prisma.service';
-export declare class NotificationService {
-    private prisma;
-    constructor(prisma: PrismaService);
-    notifyMember(order: any): Promise<void>;
-    notifyChefs(chefIds: number[], order: any): Promise<void>;
-    notifyAdmins(order: any): Promise<void>;
-}
